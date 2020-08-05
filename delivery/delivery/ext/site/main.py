c4185798d2c0b81097f5b52d75ad1c0128a24085
brenodocarmo <brenocarmosousa@gmail.com>
2020-08-05 20:53:27 -0300

    Adicionado templates e debug toolbar

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -1,14 +1,21 @@
-from flask import request, render_template
+from flask import render_template
 from flask import Blueprint
-
+from flask import current_app
 
 bp = Blueprint("site", __name__)
 
 
 @bp.route("/")
 def index():
-    return render_template(
-        "index.html",
-        name=request.args["name"]
-        # name="breno"
-    )
+    current_app.debug("Entrei na fincao main")
+    return render_template("index.html")
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/restaurantes")
+def restaurants():
+    return render_template("restaurants.html")
